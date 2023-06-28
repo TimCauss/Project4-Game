62680737e3c4ec1737b2de39ecc23b9f67d282bc
TimCauss <t.caussignac@gmail.com>
2023-06-28 14:40:05 +0200

    refactor(FemaleHero): * Change FemaleHero to Phaser.Physics.Arcade.Sprite * Create a GameObjectFactory to add FemaleHero to scenes

--- src/game/FemaleHero.ts
+++ src/game/FemaleHero.ts
@@ -1,28 +1,35 @@
 import Phaser from "phaser";
 
-import TextureKeys from "../consts/TextureKeys";
 import HeroAnimsKeys from "../consts/HeroAnimsKeys";
 
+declare global {
+  namespace Phaser.GameObjects {
+    interface GameObjectFactory {
+      fhero(
+        x: number,
+        y: number,
+        texture: string,
+        frame?: string | number
+      ): FemaleHero;
+    }
+  }
+}
+
 //Preparing First Hero Class:
-export default class FemaleHero extends Phaser.GameObjects.Container {
+export default class FemaleHero extends Phaser.Physics.Arcade.Sprite {
   //Private declarations:
-  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-  private femaleHero!: Phaser.GameObjects.Sprite;
 
-  constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y);
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    texture: string,
+    frame?: string | number
+  ) {
+    super(scene, x, y, texture, frame);
 
-    //Create Keyboard Input:
-    this.cursors = scene.input.keyboard!.createCursorKeys();
-
-    //Create FemaleHero Sprite:
-    this.femaleHero = scene.add
-      .sprite(0, 0, TextureKeys.FemaleHero)
-      .setOrigin(0.5, 1)
-      .play(HeroAnimsKeys.fHeroIdleDown);
-
-    //Add the femaleHero as a child of Container
-    this.add(this.femaleHero);
+    //
+    this.anims.play(HeroAnimsKeys.fHeroRunSide);
 
     //Creating Physics
     scene.physics.add.existing(this);
@@ -30,78 +37,29 @@ export default class FemaleHero extends Phaser.GameObjects.Container {
     //Adjusting physics body (hitbox)size, color and offset:
     const body = this.body as Phaser.Physics.Arcade.Body;
     body.debugBodyColor = 0x3cda15;
-    body.setSize(17, this.femaleHero.height * 0.6);
-    body.setOffset(-this.femaleHero.width * 0.25, -23);
+    body.setSize(17, this.height * 0.6);
+    //TODO Change HitBox Position
 
-    //setting collide with walls:
   }
 
-  preUpdate() {
-    const body = this.body as Phaser.Physics.Arcade.Body;
-
-    //KEYBOARD INPUT START:----------------------------------------------
-    //Speed setting:
-    const walkSpeed = 50;
-    const runSpeed = walkSpeed * 1.75;
-
-    if (!this.cursors || !this.femaleHero) {
-      return;
-    }
-
-    //listen keyboard input:
-    
-
-    //LEFT(Walk & Run):
-
-    if (this.cursors.left?.isDown) {
-      //Return sprite:
-      this.femaleHero.scaleX = -1;
-      if (this.cursors.shift?.isDown) {
-        this.femaleHero.anims.play(HeroAnimsKeys.fHeroRunSide, true);
-        body.setVelocity(-runSpeed, 0);
-      } else {
-        //Launch animation
-        this.femaleHero.anims.play(HeroAnimsKeys.fHeroWalkSide, true);
-        //Set velocity to left
-        body.setVelocityX(-walkSpeed);
-      }
-      //RIGHT
-    } else if (this.cursors.right?.isDown) {
-      this.femaleHero.scaleX = 1;
-      if (this.cursors.shift?.isDown) {
-        this.femaleHero.anims.play(HeroAnimsKeys.fHeroRunSide, true);
-        body.setVelocity(runSpeed, 0);
-      } else {
-        this.femaleHero.anims.play(HeroAnimsKeys.fHeroWalkSide, true);
-        body.setVelocityX(walkSpeed);
-      }
-      //UP
-    } else if (this.cursors.up?.isDown) {
-      this.femaleHero.scaleX = 1;
-      if (this.cursors.shift?.isDown) {
-        this.femaleHero.anims.play(HeroAnimsKeys.fHeroRunUp, true);
-        body.setVelocity(0, -runSpeed);
-      } else {
-        this.femaleHero.anims.play(HeroAnimsKeys.fHeroWalkUp, true);
-        body.setVelocityY(-walkSpeed);
-      }
-      //DOWN
-    } else if (this.cursors.down?.isDown) {
-      this.femaleHero.scaleX = 1;
-      if (this.cursors.shift?.isDown) {
-        this.femaleHero.anims.play(HeroAnimsKeys.fHeroRunDown, true);
-        body.setVelocity(0, runSpeed);
-      } else {
-        this.femaleHero.anims.play(HeroAnimsKeys.fHeroWalkDown, true);
-        body.setVelocityY(walkSpeed);
-      }
-    } else {
-      if (!this.femaleHero.anims.currentAnim) return;
-      const parts = this.femaleHero.anims.currentAnim.key.split("-");
-      parts[1] = "idle";
-      body.setVelocity(0, 0);
-      this.femaleHero.play(parts.join("-"));
-    }
-    //KEYBOARD INPUT END-----------------------------------------------
-  }
 }
+
+Phaser.GameObjects.GameObjectFactory.register("fhero", function (
+  this: Phaser.GameObjects.GameObjectFactory,
+  x: number,
+  y: number,
+  texture: string,
+  frame?: string | number
+) {
+  const sprite = new FemaleHero(this.scene, x, y, texture, frame);
+
+  this.displayList.add(sprite);
+  this.updateList.add(sprite);
+
+  this.scene.physics.world.enableBody(
+    sprite,
+    Phaser.Physics.Arcade.DYNAMIC_BODY
+  );
+
+  return sprite;
+});
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -4,16 +4,24 @@ import Phaser from "phaser";
 import TextureKeys from "../consts/TextureKeys";
 import MapKeys from "../consts/MapKeys";
 import SceneKeys from "../consts/SceneKeys";
-import FemaleHero from "../game/FemaleHero";
+import HeroAnimsKeys from "../consts/HeroAnimsKeys";
 import LizardGreen from "../game/LizardGreen";
 // enum import end------------------------------------
 
+import "../game/FemaleHero";
+
 export default class Game extends Phaser.Scene {
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private wallsLayer!: Phaser.Tilemaps.TilemapLayer;
+  private hero!: Phaser.Physics.Arcade.Sprite;
+
   constructor() {
     super(SceneKeys.Game);
   }
 
-  private wallsLayer!: Phaser.Tilemaps.TilemapLayer;
+  preload() {
+    this.cursors = this.input.keyboard!.createCursorKeys();
+  }
 
   create() {
     //store the width and height of the game screen:
@@ -40,8 +48,7 @@ export default class Game extends Phaser.Scene {
     // Debugging Method END--------------------------------------
 
     //Adding HERO:-----------------------------------------------
-    const hero = new FemaleHero(this, 231, 48);
-    this.add.existing(hero);
+    this.hero = this.add.fhero(231, 48, HeroAnimsKeys.fHeroIdleDown);
 
     //Adding GREEN LIZARDS :--------------------------------------
     const lizards = this.physics.add.group({
@@ -51,17 +58,89 @@ export default class Game extends Phaser.Scene {
 
     // Collider Settings-----------------------------------------
     this.physics.add.collider(lizards, this.wallsLayer);
-    this.physics.add.collider(hero, this.wallsLayer);
-    this.physics.add.collider(hero, lizards);
+    this.physics.add.collider(this.hero, this.wallsLayer);
+    this.physics.add.collider(this.hero, lizards, this.handleHeroLizardCollision, undefined, this);
 
     //Cameras Settings-------------------------------------------
-    this.cameras.main.startFollow(hero, true);
+    // this.cameras.main.startFollow(hero, true);
   }
 
-  private handleHeroLizardCollision() {
-    
+  update() {
+    //KEYBOARD INPUT START:----------------------------------------------
+    //Speed setting:
+    const walkSpeed = 50;
+    const runSpeed = walkSpeed * 1.75;
+
+    if (!this.cursors) {
+      return;
+    }
+
+    //LEFT(Walk & Run):
+    if (this.cursors.left?.isDown) {
+      //Return sprite:
+      this.hero.scaleX = -1;
+      if (this.cursors.shift?.isDown) {
+        this.hero.anims.play(HeroAnimsKeys.fHeroRunSide, true);
+        this.hero.setVelocity(-runSpeed, 0);
+      } else {
+        //Launch animation
+        this.hero.anims.play(HeroAnimsKeys.fHeroWalkSide, true);
+        //Set velocity to left
+        this.hero.setVelocityX(-walkSpeed);
+      }
+      //RIGHT
+    } else if (this.cursors.right?.isDown) {
+      this.hero.scaleX = 1;
+      if (this.cursors.shift?.isDown) {
+        this.hero.anims.play(HeroAnimsKeys.fHeroRunSide, true);
+        this.hero.setVelocity(runSpeed, 0);
+      } else {
+        this.hero.anims.play(HeroAnimsKeys.fHeroWalkSide, true);
+        this.hero.setVelocityX(walkSpeed);
+      }
+      //UP
+    } else if (this.cursors.up?.isDown) {
+      this.hero.scaleX = 1;
+      if (this.cursors.shift?.isDown) {
+        this.hero.anims.play(HeroAnimsKeys.fHeroRunUp, true);
+        this.hero.setVelocity(0, -runSpeed);
+      } else {
+        this.hero.anims.play(HeroAnimsKeys.fHeroWalkUp, true);
+        this.hero.setVelocityY(-walkSpeed);
+      }
+      //DOWN
+    } else if (this.cursors.down?.isDown) {
+      this.hero.scaleX = 1;
+      if (this.cursors.shift?.isDown) {
+        this.hero.anims.play(HeroAnimsKeys.fHeroRunDown, true);
+        this.hero.setVelocity(0, runSpeed);
+      } else {
+        this.hero.anims.play(HeroAnimsKeys.fHeroWalkDown, true);
+        this.hero.setVelocityY(walkSpeed);
+      }
+    } else {
+      if (!this.hero.anims.currentAnim) return;
+      const parts = this.hero.anims.currentAnim.key.split("-");
+      parts[1] = "idle";
+      this.hero.setVelocity(0, 0);
+      this.hero.play(parts.join("-"));
+    }
+    //KEYBOARD INPUT END-----------------------------------------------
   }
 
+  private handleHeroLizardCollision(
+    obj1: Phaser.GameObjects.GameObject,
+    obj2: Phaser.GameObjects.GameObject
+  ) {
+    const lizard = obj2 as LizardGreen;
+
+    const dx = this.hero.x - lizard.x;
+    const dy = (this.hero.y = lizard.y);
+
+    const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(200);
+
+    this.hero.setVelocity(dir.x, dir.y);
+  }
 
   // Debugging Collision Method----------------------------------
   private collisonDebug(enabled: boolean) {
